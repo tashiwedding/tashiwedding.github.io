2adac7777032ec1644b399c4f74dce3c786d2b09
tashiwedding <tashizaidi07@gmail.com>
2017-03-25 19:21:11 +0530

    Add files via upload

---
+++ js/countdown.js
@@ -0,0 +1,35 @@
+CountDownTimer('04/27/2017 07:30 PM', 'countdown');
+
+function CountDownTimer(dt, id)
+{
+    var end = new Date(dt);
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+
+            clearInterval(timer);
+            document.getElementById(id).innerHTML = 'EXPIRED!';
+
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        document.getElementById(id).innerHTML = days + ':';
+        document.getElementById(id).innerHTML += hours + ':';
+        document.getElementById(id).innerHTML += minutes + ':';
+        document.getElementById(id).innerHTML += seconds + '';
+    }
+
+    timer = setInterval(showRemaining, 1000);
+}
